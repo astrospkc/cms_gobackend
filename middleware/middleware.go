318347628dcb187fc3083e2a7c2af8fb02e20ab6
Punam <punamkumari399@gmail.com>
2025-04-09 23:30:54 +0530

    auth middleware recorrect

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -8,28 +8,33 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func AuthenticateMiddleware(c *fiber.Ctx) error {
-	tokenString := c.Cookies("token")
-	if tokenString == "" {
+func AuthenticateMiddleware() fiber.Handler {
+	
+	return func(c *fiber.Ctx) error {
+
+		tokenString := c.Cookies("token")
+		if tokenString == "" {
 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 				"error": "Missing token",
 			})
-	}
-	token, err := jwt.Parse(tokenString, func ()token *jwt.Token) (interface{}, error)  {
+		}
+		token, err := jwt.Parse(tokenString, func (token *jwt.Token) (interface{}, error) {
 		if _, ok:= token.Method.(*jwt.SigningMethodHMAC); !ok{
 			return nil, fmt.Errorf("Unexpected signing method")
 		}
 		return []byte(os.Getenv("JWT_SECRET")), nil
-	})
+		})
 
-	if err!=nil || !token.Valid {
+		if err!=nil || !token.Valid {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 				"error": "Expired token",
 			})
-	}
+		}
 
-	claims := token.Claims.(jwt.MapClaims)
-	c.Locals("user", claims)
-	return c.Next()
+		claims := token.Claims.(jwt.MapClaims)
+		c.Locals("user", claims)
+		return c.Next()
+	}
+	
 
 }
